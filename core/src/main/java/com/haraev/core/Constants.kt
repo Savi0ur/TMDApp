de9e8ae0b67a944b256c256eac0b69b42022c7a6
Haraev Anton <haraevanton@yandex.ru>
2020-03-17 22:13:01 +0300

    Обновлены зависимости, добавлен API ключ

---
+++ core/src/main/java/com/haraev/core/Constants.kt
@@ -0,0 +1,11 @@
+package com.haraev.core
+
+/**
+ * API ключ для запросов к https://api.themoviedb.org/
+ */
+const val TMDB_API_KEY = BuildConfig.TMDB_API_KEY
+
+/**
+ * Базовый URL для запросов
+ */
+const val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
